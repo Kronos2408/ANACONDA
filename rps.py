4ff2c51a89c203657178f8c21caf0e48b7235d52
Kronos2408 <47881253+Kronos2408@users.noreply.github.com>
2019-02-25 10:06:15 +0530

    Add files via upload

--- rps.py
+++ rps.py
@@ -1,21 +1,24 @@
 import random
 l=["r","p","s"]
+while True:
+	#take input from user
+	u=input("enter your choice,press n to discontinue")
+	#to exit
+	if u=="n":
+		print("exit")
+		exit(0)
+	#input from computer
+	c=random.choice(l)
+	print("computer chooses",c)
 
-#take input from user
-u=input("enter your choice")
-
-#input from computer
-c=random.choice(l)
-print("computer chooses",c)
-
-#compare the user and computer choice
-if u==c:
-	print("tie")
-elif u=="r" and c=="p":
-	print("comp wins")
-elif u=="p" and c=="s":
-	print("comp wins")
-elif u=="s" and c=="r":
-	print("comp wins")
-else:
-	print("u win")
+	#compare the user and computer choice
+	if u==c:
+		print("tie")
+	elif u=="r" and c=="p":
+		print("comp wins")
+	elif u=="p" and c=="s":
+		print("comp wins")
+	elif u=="s" and c=="r":
+		print("comp wins")
+	else:
+		print("u win")
